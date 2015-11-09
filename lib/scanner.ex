8096831a4e7fde5a7db1444ef82950aabaa381bb
karmajunkie <keith.gaddis@gmail.com>
2015-11-09 10:22:42 -0600

    Don't do extra work and improve interface
    Get the repeated scrape test passing, add a new API for adding a
    new scraper

--- lib/scanner.ex
+++ lib/scanner.ex
@@ -2,20 +2,6 @@ defmodule Scanner do
   use GenServer
 
   #client interface
-  def start_link(args) do
-    GenServer.start_link(__MODULE__, args, name: __MODULE__)
-  end
-
-  # def handle_init(args) do
-  #   {:ok, args}
-  # end
-
-  def handle_call({:add_page, url},  {from_pid, ref}, {scrapers, requester}) do
-    requester.fetch(from_pid, url)
-
-    {:reply, :ok, {scrapers, requester}}
-  end
-
   def add_page(nil) do
     :error
   end
@@ -24,4 +10,34 @@ defmodule Scanner do
     GenServer.call(__MODULE__, {:add_page, url})
   end
 
+  def add_scraper(scraper) do
+    GenServer.call(__MODULE__, {:add_scraper, scraper})
+  end
+
+  #GenServer impl
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(args \\ %{}) do
+    {:ok,
+     args |> Map.put(:table, :ets.new(:urls, [:set, :protected]))
+    }
+  end
+
+  def handle_call({:add_scraper,  scraper},_, state) do
+    {:ok,
+     state |> Map.put(scrapers: Enum.concat(state.scrapers, scraper))
+    }
+  end
+
+  def handle_call({:add_page, url},  _, state) do
+    elm = :ets.lookup(state.table, url)
+    if Enum.empty?(elm) do
+      state.requester.fetch(url)
+      :ets.insert(state.table, {url, true})
+    end
+
+    {:reply, :ok, state}
+  end
 end
--- test/scanner_test.exs
+++ test/scanner_test.exs
@@ -10,17 +10,28 @@ defmodule ScannerTest do
       {:ok, parent}
     end
 
-    def fetch(pid, url) do
-      send(pid, {:fetched, url})
+    def handle_cast({:fetch, url}, parent_pid) do
+      send(parent_pid, {:fetched, url})
+      {:noreply, parent_pid}
+    end
+    def fetch(url) do
+      GenServer.cast(__MODULE__, {:fetch, url})
     end
   end
 
-  defmodule AgreeableTestScraper do
+  defmodule DisagreeableTestScraper do
     def valid_for_retrieval? do
       false
     end
   end
+
   defmodule TestScraper do
+    use GenServer
+
+    def start_link(parent) do
+      GenServer.start_link(__MODULE__, %{parent: parent}, name: __MODULE__)
+    end
+
     def valid_for_retrieval?(_url) do
       true
     end
@@ -30,30 +41,36 @@ defmodule ScannerTest do
     end
   end
 
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   setup do
     TestRequester.start_link(self)
-    Scanner.start_link({[TestScraper], TestRequester })
+    Scanner.start_link(%{requester: TestRequester, scrapers: [] })
     :ok
   end
 
-  test "adding nothing adds... nothing" do
+  test "adding nil is an error" do
       assert :error=Scanner.add_page(nil)
   end
 
-  test "adding a url sends it to the scrape queue" do
+  test "adding a url sends it to the request engine" do
     test_url="http://foo.com"
     Scanner.add_page(test_url)
     assert_received {:fetched, ^test_url}, "first message not received"
   end
 
-  test "adding a url a second time does not scrape it again" do
+  test "adding a url a second time does not send it to the request engine a second time" do
     test_url="http://foo.com"
     Scanner.add_page(test_url)
     Scanner.add_page(test_url)
     assert_received {:fetched, ^test_url}, "first message not received"
     refute_received {:fetched, ^test_url}, "Second message received when it shouldn't be"
+  end
+
+  test "adding a scraper" do
+    #how to have the scraper call back to test here?
+    assert :ok=Scanner.add_scraper(TestScraper)
+
   end
 
 end
